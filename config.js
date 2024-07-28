db4d95ce6f45c886d6d7d6cf073f91d006b08264
FarhanAnsari <149857451+farhanansari888@users.noreply.github.com>
2024-07-28 23:11:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Only For You",
+    introTitle: 'My Dear Panda',
+    introDesc: `The Earth is strange
+    But why do I keep going the wrong way?
+    Lost in my lonely heart 
+    Behind the word love is compassion`,
+    btnIntro: '^^HiHi^^',
+    title: 'Was it love at first site 🥰',
+    desc: 'Was I drunk the moment I saw that smile?',
+    btnYes: 'Love it <3',
+    btnNo: 'No :3',
+    question: 'There are more than & billion peopke in the world, why do you love me <3',
+    btnReply: ' Sent to you<3',
+    reply: 'Love then Love, If you dont Love then Love<33333333',
+    mess: 'I know 🥰. Love you so much 😘😘',
+    messDesc: 'We Will Meet Soon ❤️😘.',
+    btnAccept: 'Okiiiii Love You <3',
+    messLink: href='https://t.me/farhanansari_888' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
